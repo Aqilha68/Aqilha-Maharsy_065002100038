17bbe56bf6bf1eeffca9fa56a58ac3553609a81b
Aqilha68 <91168188+Aqilha68@users.noreply.github.com>
2021-09-22 12:46:01 +0700

    Add files via upload

---
+++ prak algo Aqilha Maharsy.py
@@ -0,0 +1,32 @@
+print(" @@@    @@@     @  @      @    @   @@@ ")
+print("@   @  @   @    @  @      @    @  @   @")
+print("@   @  @   @    @  @      @    @  @   @")
+print("@@@@@  @@  @    @  @      @@@@@@  @@@@@")
+print("@   @  @  @@    @  @      @    @  @   @")
+print("@   @  @   @@   @  @      @    @  @   @")
+print("@   @   @@@  @  @  @@@@@  @    @  @   @")
+print("Aqilha Maharsy")
+print('nama','"Aqilha Maharsy"')
+
+
+import math
+
+x=input("Masukkan angka pertama : ")
+
+p1=x.split(",")
+
+y=input("Masukkan angka kedua : ")
+
+p2=y.split(",")
+
+jarak = math.sqrt( ((int(p1[0])-int(p2[0]))**2)+((int(p1[1])-int(p2[1]))**2) )
+
+print ("Jarak antara ", x,"dan", y, "adalah", jarak)
+
+                 
+#huruf kecil ke kapital
+string = "suka belajar algo"
+print
+cara= string.upper()
+print(cara) 
+                
