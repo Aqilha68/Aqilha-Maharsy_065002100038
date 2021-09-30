0d57685a90cccd6e75dc921c42d66091fb83f5d2
Aqilha68 <91168188+Aqilha68@users.noreply.github.com>
2021-09-30 12:47:30 +0700

    Add files via upload

---
+++ prak algo Orkom 2.py
@@ -0,0 +1,18 @@
+print(" @@@    @@@     @  @      @    @   @@@ ")
+print("@   @  @   @    @  @      @    @  @   @")
+print("@   @  @   @    @  @      @    @  @   @")
+print("@@@@@  @@  @    @  @      @@@@@@  @@@@@")
+print("@   @  @  @@    @  @      @    @  @   @")
+print("@   @  @   @@   @  @      @    @  @   @")
+print("@   @   @@@  @  @  @@@@@  @    @  @   @")
+
+a=int(input("Masukkan bilangan pertama = "))
+b=int(input("Masukkan bilangan kedua = "))
+c=int(input("Masukkan bilangan ketiga = "))
+
+if(a>b) and (a>c) :
+    print("Bilangan terbesar adalah= ",a)
+elif (b>a) and (b>c) :
+    print("Bilangan terbesar adalah = ",b)
+elif (c>a) and (c>b) :
+    print("Bilangan terbesar adalah = ",c)
