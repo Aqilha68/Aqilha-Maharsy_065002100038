7f10e976c32f302d046126969011c102e2d2ac93
Aqilha68 <91168188+Aqilha68@users.noreply.github.com>
2021-10-06 08:49:07 +0700

    Add files via upload

---
+++ prak algo 3 orkom.py
@@ -0,0 +1,15 @@
+print(" @@@    @@@     @  @      @    @   @@@ ")
+print("@   @  @   @    @  @      @    @  @   @")
+print("@   @  @   @    @  @      @    @  @   @")
+print("@@@@@  @@  @    @  @      @@@@@@  @@@@@")
+print("@   @  @  @@    @  @      @    @  @   @")
+print("@   @  @   @@   @  @      @    @  @   @")
+print("@   @   @@@  @  @  @@@@@  @    @  @   @")
+awal = int(input('Masukkan angka awal : '))
+akhir = int(input('Masukkan angka akhir : '))
+
+while awal<=akhir:
+    print (awal,akhir)
+    awal = awal+1
+    akhir = akhir-1
+    
