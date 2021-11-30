1c59abd69e6731ac0ecb922dd3da9b9f9f56d693
Aqilha68 <91168188+Aqilha68@users.noreply.github.com>
2021-11-30 10:11:12 +0700

    Add files via upload

---
+++ Prak Algo 9.1.py
@@ -0,0 +1,8 @@
+print('tuple 1: ')
+a=('1021','1022', '1023', '1024', '1025', '1026', '1027', '1028', '1029', '1030', '1030')
+b = ", ".join(a[0:3])
+c = ", ".join(a[4:7])
+d= ", ".join(a[8:11])
+print((b,c,d))
+print('rata rata nilai pada masing-masing tuple: ')
+print ([sum(map(float, filter(None, a[0:3])))/(len(a)-1),(sum(map(float, filter(None, a[4:7])))/(len(a)-1)),(sum(map(float, filter(None, a[8:11])))/(len(a)-1))])  
